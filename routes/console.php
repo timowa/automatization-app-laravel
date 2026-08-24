25b6944cf5004d7128680bc98a15f8c18815deab
timowa <timofejshhetinin@gmail.com>
2026-08-24 11:38:37 +0700

    правка

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::command('vk:check-tokens')->daily();
+Schedule::command('vk:sync-users')->daily();
+Schedule::command('vk:publish-loop-stories')->cron('0 3 */3 * *');
+
